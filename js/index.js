c49ecfff5421f810feb4c29edbceab38ca58fb75
Fábio Costa Silva <developerfabiocsilva@gmail.com>
2025-05-27 00:43:24 -0300

    menu hamburguer

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+//     var navbarToggler = document.querySelector(".navbar-toggler");
+//     var navbarCollapse = document.querySelector("#navbarNav");
+
+//     navbarToggler.addEventListener("click", function() {
+//         navbarCollapse.classList.toggle("show");
+//     });
+// });
+
+// window.onload = function() {
+//     var navbarToggler = document.querySelector(".navbar-toggler");
+//     navbarToggler.addEventListener("click", function() {
+//         var navbarCollapse = document.querySelector("#navbarNav");
+//         new bootstrap.Collapse(navbarCollapse, { toggle: true });
+//     });
+// };
+
+
+document.addEventListener("DOMContentLoaded", function () {
     var navbarToggler = document.querySelector(".navbar-toggler");
     var navbarCollapse = document.querySelector("#navbarNav");
 
-    navbarToggler.addEventListener("click", function() {
+    navbarToggler.addEventListener("click", function () {
         navbarCollapse.classList.toggle("show");
     });
 });
