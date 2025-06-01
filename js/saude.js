91adb63001dd759c628353b1a07d2bd2222cb9c0
Fábio Costa Silva <developerfabiocsilva@gmail.com>
2025-06-01 16:56:07 -0300

    menu de hamburguer no arquivo saude.css, saude.html

--- js/saude.js
+++ js/saude.js
@@ -1,25 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbarToggler = document.querySelector(".navbar-toggler");
     const navbarCollapse = document.querySelector("#navbarNav");
-    const navLinks = document.querySelectorAll("#navbarNav .nav-link");
 
-    // Alternar a classe 'show' corretamente no mobile
     navbarToggler.addEventListener("click", function () {
-        const isExpanded = navbarToggler.getAttribute("aria-expanded") === "true";
-        if (isExpanded) {
-            navbarCollapse.classList.remove("show");
-        } else {
-            navbarCollapse.classList.add("show");
-        }
-    });
-
-    // Fechar ao clicar em qualquer link no mobile
-    navLinks.forEach(function (link) {
-        link.addEventListener("click", function () {
-            navbarCollapse.classList.remove("show");
+        new bootstrap.Collapse(navbarCollapse, {
+            toggle: true
         });
     });
 });
+
 
 
 
